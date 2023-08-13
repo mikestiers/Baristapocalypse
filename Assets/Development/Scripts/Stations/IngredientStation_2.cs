235508eea8a0e3dfc609f59b6ac23bab6b5cc09a
teanbes <teanbes@gmail.com>
2023-08-13 14:27:18 -0400

    New Ingredient station that holds all the ingredients
    - Created new Ingredient Station that handle multiple ingredients

--- Assets/Development/Scripts/Stations/IngredientStation_2.cs
+++ Assets/Development/Scripts/Stations/IngredientStation_2.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class IngredientStation_2 : MonoBehaviour
+public class IngredientStation_2 : BaseStation, IHasProgress
 {
-    // Start is called before the first frame update
-    void Start()
+    public event EventHandler<IHasProgress.OnProgressChangedEventArgs> OnProgressChanged;
+
+    [SerializeField] private IngredientSO[] ingredientListSO;
+    [SerializeField] private ParticleSystem interactParticle;
+    [SerializeField] private TextMeshPro currentIngredienIndicator;
+
+    private IngredientSO currentIngredient;
+    private int ingredientListSOIndex;
+
+    private void Start()
     {
-        
+        ingredientListSOIndex = 0;
+        currentIngredient = ingredientListSO[ingredientListSOIndex];
+        currentIngredienIndicator.text = currentIngredient.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Interact(PlayerStateMachine player)
     {
-        
+        if (!HasIngredient())
+        {
+            if (player.HasIngredient()) //check if player has ingredient
+            {
+                if (player.GetIngredient().CompareTag("Milk")) //check if player has milk ingredient
+                {
+                    player.GetIngredient().DestroyIngredient(); //destroy ingredient
+                }
+            }
+            else
+            {
+                Ingredient.SpawnIngredient(currentIngredient, player);
+                player.GetNumberOfIngredients();
+                SoundManager.Instance.PlayOneShot(SoundManager.Instance.audioClipRefsSO.interactStation);
+                interactParticle.Play();
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public override void InteractAlt(PlayerStateMachine player)
+    {
+        if (ingredientListSOIndex >= ingredientListSO.Length - 1)
+        {
+            ingredientListSOIndex = 0;
+        }
+        else
+        {
+            ingredientListSOIndex++;
+        }
+        currentIngredient = ingredientListSO[ingredientListSOIndex];
+        currentIngredienIndicator.text = currentIngredient.ToString();
+        OnProgressChanged?.Invoke(this, new IHasProgress.OnProgressChangedEventArgs
+        {
+            progressNormalized = ingredientListSOIndex
+        });
     }
 }
