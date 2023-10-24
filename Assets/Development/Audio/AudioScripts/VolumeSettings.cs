10d878b4c98bda0625d0b3689597ba8185c474fc
ProfChess <122888087+ProfChess@users.noreply.github.com>
2023-10-24 14:58:56 -0300

    Audio Settings
    Added main menu audio control. 2 Sliders for music and SFX that work separately. Will add to in game settings soon.

---
+++ Assets/Development/Audio/AudioScripts/VolumeSettings.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    private void Start()
+    {
+        SetMusicVolume();
+    }
+
+    public void SetMusicVolume()
+    {
+        float Musvolume = musicSlider.value;
+        myMixer.SetFloat("Music", Mathf.Log10(Musvolume)*20);
+    }
+
+    public void SetSFXVolume()
+    {
+        float SFXvolume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(SFXvolume) * 20);
+    }
+}
