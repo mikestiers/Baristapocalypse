8146d21959be667539104f97f44e9e347249ba2a
mikestiers <mike.stiers@gmail.com>
2023-12-08 08:37:55 -0500

    Cleaned up variables for presentation

--- Assets/Development/Scripts/Stations/BlackHole.cs
+++ Assets/Development/Scripts/Stations/BlackHole.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class BlackHole : MonoBehaviour
 {
     public float gravitationalPull = 10f; // Adjustable gravitational pull strength
-    public float centerForce = 3.7f;
-    public float rightForce = 3.5f;
+    public float centerForceScale = 3.7f;
+    public float rightForceScale = 3.5f;
     private void OnValidate()
     {
-        if (centerForce <= rightForce)
+        if (centerForceScale <= rightForceScale)
         {
             Debug.LogError("centerForce needs to be greater than rightForce");
-            centerForce = 3.7f;
-            rightForce = 3.5f;
+            centerForceScale = 3.7f;
+            rightForceScale = 3.5f;
         }
     }
 
@@ -28,16 +28,15 @@ public class BlackHole : MonoBehaviour
         Debug.Log("In the blackhole");
         targetRigidbody.useGravity = false;
         Vector3 directionToCenter = transform.position - targetRigidbody.position;
-        //float distance = directionToCenter.magnitude;
         float distance = Vector3.Distance(transform.position, targetRigidbody.position);
 
-        // Middle Force
-        Vector3 gravitationalForce = directionToCenter.normalized * (gravitationalPull / distance); // Force decreases with distance
+        // Center Force
+        Vector3 centerForce = directionToCenter.normalized * (gravitationalPull / distance); // Force decreases with distance
 
         // Right and Up Force
         targetRigidbody.transform.LookAt(transform.position);
         Vector3 rightForce = (targetRigidbody.transform.up + targetRigidbody.transform.right) * (gravitationalPull / distance);
-        targetRigidbody.AddForce((gravitationalForce * 3.7f) + (rightForce * 3.5f), ForceMode.Force);
+        targetRigidbody.AddForce((centerForce * centerForceScale) + (rightForce * rightForceScale), ForceMode.Force);
 
         targetRigidbody.velocity = Vector3.ClampMagnitude(targetRigidbody.velocity, 5.0f);
     }
