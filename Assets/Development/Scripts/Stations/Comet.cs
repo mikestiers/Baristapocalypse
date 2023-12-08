41c7d32db7039f46905e28862acad26f82e2dc2d
mikestiers <mike.stiers@gmail.com>
2023-12-08 10:38:09 -0500

    Material for blackhole,  Comet
    Made a material so the black hole was visible and slightly transparent
    Created a comet

--- Assets/Development/Scripts/Stations/Comet.cs
+++ Assets/Development/Scripts/Stations/Comet.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestGravity : MonoBehaviour
+public class Comet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody>().AddForce(transform.forward * 500);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
