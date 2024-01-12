cc014fd4d6247c8707f6e65c80d702496f567b0e
mikestiers <mike.stiers@gmail.com>
2024-01-12 01:14:08 -0500

    New order / crafting UI
    - Added working example of one slider bar that incorporates cumulative, potential, and target attribute values together
    - Added sample art asset for UI

--- Assets/Development/Scripts/UI/OrderStats.cs
+++ Assets/Development/Scripts/UI/OrderStats.cs
@@ -6,11 +6,10 @@ using UnityEngine.UI;
 public class OrderStats : MonoBehaviour
 {
     [Header("Customer Order")]
-    [SerializeField] private Slider bitternessSlider;
-    [SerializeField] private Slider sweetnessSlider;
-    [SerializeField] private Slider strengthSlider;
     [SerializeField] private Slider temperatureSlider;
+    [SerializeField] private Slider sweetnessSlider;
     [SerializeField] private Slider spicinessSlider;
+    [SerializeField] private Slider strengthSlider;
     [SerializeField] private Text customerNumberText;
     
     [Header("Customer Review")]
@@ -26,11 +25,10 @@ public class OrderStats : MonoBehaviour
     public void Initialize(CustomerBase customer)
     {
         orderOwner = customer;
-        bitternessSlider.value = customer.coffeeAttributes.GetBitterness() * .10f;
-        sweetnessSlider.value = customer.coffeeAttributes.GetSweetness() * .10f;
-        strengthSlider.value = customer.coffeeAttributes.GetStrength() * .10f;
         temperatureSlider.value = customer.coffeeAttributes.GetTemperature() * .10f;
+        sweetnessSlider.value = customer.coffeeAttributes.GetSweetness() * .10f;
         spicinessSlider.value = customer.coffeeAttributes.GetSpiciness() * .10f;
+        strengthSlider.value = customer.coffeeAttributes.GetStrength() * .10f;
         customerNumberText.text = customer.customerNumber.ToString();
     }
 }
