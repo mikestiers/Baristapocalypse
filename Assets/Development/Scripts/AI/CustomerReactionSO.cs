2b940d2a0fa09751d4e3c1ff85302ee7b7209a40
ParkDouglas <116034374+ParkDouglas@users.noreply.github.com>
2024-01-16 11:15:39 -0500

    sciptableObject type reaction

---
+++ Assets/Development/Scripts/AI/CustomerReactionSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "CustomerReaction", menuName = "Reaction SO")]
+public class CustomerReactionSO : ScriptableObject
+{
+    public Image Image;
+}
