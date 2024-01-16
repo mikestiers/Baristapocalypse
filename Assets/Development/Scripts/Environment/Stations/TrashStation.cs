8b6f4aff716c1c877ae58dc74474871cc4988fad
teanbes <teanbes@gmail.com>
2024-01-16 10:30:40 -0500

    Work In progress

--- Assets/Development/Scripts/Environment/Stations/TrashStation.cs
+++ Assets/Development/Scripts/Environment/Stations/TrashStation.cs
@@ -1,26 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class TrashStation : BaseStation
 {
     [SerializeField] private ParticleSystem interactParticle;
+    private Ingredient ingredient;
+    private PlayerController playerControllerInstance;
+
+
     public override void Interact(PlayerController player)
     {
-        if (player.GetNumberOfIngredients() >= 1)
+        playerControllerInstance = player;
+
+        // Temporary thile hold points are fixed
+        if (player.HasIngredient())
         {
-            for (int i = 0; i < player.ingredientHoldPoints.Length; i++)
-            {
-                Transform holdPoint = player.ingredientHoldPoints[i];
-                if (holdPoint.childCount > 0)
-                {
-                    Ingredient ingredient = holdPoint.GetComponentInChildren<Ingredient>();
-                    Ingredient.DestroyIngredient(ingredient);
-                    SoundManager.Instance.PlayOneShot(SoundManager.Instance.audioClipRefsSO.interactStation);
-                    //interactParticle.Play();
-                }
-            }
+            Debug.Log("trashinggggg");
+            ingredient = player.GetIngredient();
+            InteractServerRpc();
         }
+
+        //if (player.GetNumberOfIngredients() >= 1)
+        //{
+        //    for (int i = 0; i < player.ingredientHoldPoints.Length; i++)
+        //    {
+        //        Transform holdPoint = player.ingredientHoldPoints[i];
+        //        if (holdPoint.childCount > 0)
+        //        {
+        //            ingredient = holdPoint.GetComponentInChildren<Ingredient>();
+        //            InteractServerRpc();
+        //            //Ingredient.DestroyIngredient(ingredient);
+        //            SoundManager.Instance.PlayOneShot(SoundManager.Instance.audioClipRefsSO.interactStation);
+        //            //interactParticle.Play();
+        //        }
+        //    }
+        //}
     }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void InteractServerRpc()
+    {
+        InteractClientRpc();
+    }
+
+    [ClientRpc]
+    private void InteractClientRpc()
+    {
+
+        //Ingredient.DestroyIngredient(ingredient);
+        //Ingredient.DestroyIngredient(ingredient);
+    }
+
 }
 
