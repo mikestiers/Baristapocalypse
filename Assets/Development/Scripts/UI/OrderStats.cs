f3e6d0c1251bd04b4711854e5773b630f165ba5d
mikestiers <mike.stiers@gmail.com>
2024-01-19 19:57:11 -0500

    Removed UI reference to order responsibility
    Was left over from bug cleanup

--- Assets/Development/Scripts/UI/OrderStats.cs
+++ Assets/Development/Scripts/UI/OrderStats.cs
@@ -7,7 +7,6 @@ public class OrderStats : MonoBehaviour
 {
     [Header("Customer Order")]
     [SerializeField] public Text customerNumberText;
-    [SerializeField] public Text orderResponsibilityText;
     [SerializeField] public OrderStatsSegments temperatureSegments;
     [SerializeField] public OrderStatsSegments sweetnessSegments;
     [SerializeField] public OrderStatsSegments spicinessSegments;
@@ -16,27 +15,15 @@ public class OrderStats : MonoBehaviour
     [Header("Customer Review")]
     [SerializeField] private GameObject customerReview;
     [SerializeField] private CustomerBase orderOwner;
-    [SerializeField] private PlayerController orderResponsibility;
 
     public CustomerBase GetOrderOwner()
     {
         return orderOwner;
     }
 
-    public PlayerController GetOrderResponsibility()
-    {
-        return orderResponsibility;
-    }
-
-    public void SetOrderResponsibility(PlayerController responsibility)
-    {
-        orderResponsibility = responsibility;
-    }
-
     public void Initialize(CustomerBase customer)
     {
         orderOwner = customer;
-        orderResponsibilityText.text = orderResponsibility.ToString();
         customerNumberText.text = customer.customerNumber.ToString();
         temperatureSegments.targetAttributeValue = customer.coffeeAttributes.GetTemperature();
         sweetnessSegments.targetAttributeValue = customer.coffeeAttributes.GetSweetness();
