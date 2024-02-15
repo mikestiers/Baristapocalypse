65ca7a29a58eb11de207d4faad5bdccc75372eb0
Brett Penney <103518828+BPenney98@users.noreply.github.com>
2024-02-15 16:58:32 -0700

    WIP CleanupBot
    Working on getting the bot to switch between two states and getting rid of the trash.

---
+++ Assets/Development/Scripts/AI/CleanupBot.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class CleanupBot : MonoBehaviour
+{
+    
+    public UnityEvent<BotState, GameObject> onStateChanged;
+    NavMeshAgent agent;
+
+    //Keep reference to player or target if aggro'd.
+    public GameObject target;
+    public Transform playerTransform;
+    public float distance = 20f;
+
+    public enum BotState
+    {
+        Roam,
+        Cleanup
+    }
+
+    [SerializeField] BotState _currentState;
+
+    public BotState currentState
+    {
+        get { return _currentState; }
+        set
+        {
+            _currentState = value;
+            onStateChanged.Invoke(_currentState, gameObject);
+        }
+    }
+
+
+    public GameObject[] path;
+    public int pathIndex = 0;
+    public float distToNextNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+
+        if (path.Length <= 0)
+        {
+            path = GameObject.FindGameObjectsWithTag("Node");
+        }
+
+        if (_currentState == BotState.Cleanup)
+        {
+            target = GameObject.FindGameObjectWithTag("Mess");
+
+            if (target)
+                agent.SetDestination(target.transform.position);
+
+        }
+
+        if (distToNextNode <= 0)
+        {
+            distToNextNode = 0.5f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentState == BotState.Roam)
+        {
+
+            if (target)
+                Debug.DrawLine(transform.position, target.transform.position, Color.blue);
+
+            if (agent.remainingDistance < distToNextNode)
+            {
+                pathIndex++;
+
+                pathIndex %= path.Length;
+
+                target = path[pathIndex];
+            }
+
+
+        }
+        if (_currentState == BotState.Cleanup)
+        {
+            if (target.gameObject.tag == "Node")
+            {
+                target = GameObject.FindGameObjectWithTag("Mess");
+            }
+
+        }
+
+        if (target)
+            agent.SetDestination(target.transform.position);
+
+        float distToMess = Vector3.Distance(transform.position, playerTransform.transform.position);
+
+        if (distToMess > distance)
+        {
+            target = path[pathIndex];
+            _currentState = BotState.Roam;
+        }
+        if (distToMess < distance)
+        {
+            _currentState = BotState.Cleanup;
+        }
+    }
+}
