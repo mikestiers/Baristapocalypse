342559db8af8bbe538bd69ed213e766502a0c77a
Samori79 <samori79@gmail.com>
2024-02-15 17:16:03 -0500

    ouline error test
    help me micheal ;_;

---
+++ Assets/Art/VFX/Samori's work/outline atempts/code/BackFaceOutLines.hlsl
@@ -0,0 +1,34 @@
+#ifndef BACKFACEOUTLINES_INCLUDED
+#define BACKFACEOUTLINES_INCLUDED
+
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+struct Attributes {
+    float4 positionOS   : POSITION;
+    float3 normalOS     : NORMAL;
+};
+
+struct VertexOutput {
+    float4 positionCS   : SV_POSITION;
+};
+
+float _Thickness;
+float4 _Color;
+
+VertexOutput Vertex(Attributes input){
+    VertexOutput output = (VertexOutput)0;
+
+    float3 normalOS = input.normalOS;
+
+    float3 posOS = input.positionOS.xyz + normalOS * _Thickness;
+    output.positionCS = GetVertexPositionInputs(posOS).positionCS;
+
+    return output;
+}
+
+float4 Fragment(VertexOutput input) : SV_Target {
+    return _Color;
+}
+
+#endif
---
+++ Assets/Art/VFX/Samori's work/outline atempts/code/BackFaceOutLines.shader
@@ -0,0 +1,28 @@
+Shader "Outlines/BackFaceOutLines"{
+    Properties {
+        _Thickness("Thickness", Float) = 1
+        _Color("Color", Color) = (1,1,1,1)
+    }
+    SubShader{
+        Tags { "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline" }
+
+        Pass{
+           Name "Outlines"
+
+           Cull Front
+
+            HLSLPROGRAM
+
+                #pragma prefer_hlslcc gles
+                #pragma exclude_renderers d3d11_9x
+
+                #pragma vertex Vertex
+                #pragma fragment fragment
+
+                #include "BackFaceOutLines.hlsl"
+
+         
+            ENDHLSL
+        }
+    }
+}
