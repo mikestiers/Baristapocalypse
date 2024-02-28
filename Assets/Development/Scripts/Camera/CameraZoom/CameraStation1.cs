caa0c5ffe9b4794f0cb8e374795b2b6a2db6f96d
ParkDouglas <116034374+ParkDouglas@users.noreply.github.com>
2024-02-27 23:55:16 -0500

    camera zoom

---
+++ Assets/Development/Scripts/Camera/CameraZoom/CameraStation1.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraStation1 : MonoBehaviour
+{
+
+    [SerializeField] private CinemachineVirtualCamera m_Camera;
+
+   
+    private int insidePriority = 20;
+
+    
+    private int defaultPriority = 0;
+
+
+    private void Start()
+    {
+        m_Camera.Priority = defaultPriority;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.CompareTag("Player"))
+        {
+            m_Camera.gameObject.SetActive(true);
+
+            m_Camera.Priority = insidePriority;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            m_Camera.gameObject.SetActive(false);
+
+            m_Camera.Priority = defaultPriority;
+        }
+    }
+}
