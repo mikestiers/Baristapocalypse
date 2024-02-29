1071f581d70f71f4e10702e63ab7b244d8460971
jeffrey-tong <jeffreytong41@gmail.com>
2024-02-29 12:13:03 -0500

    duplicate ingredients added to host

--- Assets/Development/Scripts/UI/IngredientSelectionUI.cs
+++ Assets/Development/Scripts/UI/IngredientSelectionUI.cs
@@ -159,6 +159,7 @@ public class IngredientSelectionUI : BaseStation
 
     public void AddIngredient()
     {
+        if (!IsLocalPlayer) return;
         //EventSystem.current.SetSelectedGameObject(null);
         SoundManager.Instance.PlayOneShot(SoundManager.Instance.audioClipRefsSO.interactStation);
         if (OrderManager.Instance.brewingStations[player.currentBrewingStation].TryAddIngredient(currentIngredient))
