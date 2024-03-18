6e651244e601ddb06680bc8828fbae67881dca8f
jeffrey-tong <jeffreytong41@gmail.com>
2024-03-18 13:19:28 -0400

    Gravity station network fix

--- Assets/Development/Scripts/Environment/Physics/GravityStation.cs
+++ Assets/Development/Scripts/Environment/Physics/GravityStation.cs
@@ -16,7 +16,7 @@ public class GravityStation : NetworkBehaviour
     {
         if (other.GetComponent<PlayerController>())
         {
-            InputManager.Instance.InteractEvent += DeactivateStormEventServerRpc;
+            InputManager.Instance.InteractEvent += DeactivateStormEvent;
         }
     }
 
@@ -24,72 +24,67 @@ public class GravityStation : NetworkBehaviour
     {
         if (other.GetComponent<PlayerController>())
         {
-            InputManager.Instance.InteractEvent -= DeactivateStormEventServerRpc;
+            InputManager.Instance.InteractEvent -= DeactivateStormEvent;
         }
+    }
+
+    private void DeactivateStormEvent()
+    {
+        DeactivateStormEventServerRpc();
     }
 
     [ServerRpc(RequireOwnership = false)]
     public void DeactivateStormEventServerRpc()
     {
-        DeactivateStormEventClientRpc();
+        if (GameManager.Instance.isEventActive.Value && GameManager.Instance.isGravityStorm.Value)
+        {
+            GameManager.Instance.isEventActive.Value = false;
+            GameManager.Instance.isGravityStorm.Value = false;
+            DeactivateStormEventClientRpc();
+        }
     }
 
     [ClientRpc]
     private void DeactivateStormEventClientRpc()
     {
-        DeactivateStormEvent();
-    }
 
-    private void DeactivateStormEvent()
-    {
-        if(GameManager.Instance.isEventActive.Value && GameManager.Instance.isGravityStorm.Value) {
-            GameManager.Instance.isEventActive.Value = false;
-            GameManager.Instance.isGravityStorm.Value = false;
-
-            RandomEventBase randomEvent = GameManager.Instance.currentRandomEvent;
-            if (randomEvent == null) return;
-            GravityStorm gravityStorm = randomEvent.gameObject.GetComponent<GravityStorm>();
-            if (gravityStorm != null)
+        RandomEventBase randomEvent = GameManager.Instance.currentRandomEvent;
+        if (randomEvent == null) return;
+        GravityStorm gravityStorm = randomEvent.gameObject.GetComponent<GravityStorm>();
+        if (gravityStorm != null)
+        {
+            GameManager.Instance.randomEventEffects.TurnOnOffEventEffectServerRpc(false);
+            // Populate objectsToMoveList before conversion
+            gravityStorm.objectsToMoveList.Clear(); // Clear the list before populating
+            foreach (var obj in gravityStorm.objectsToMove)
             {
-                GameManager.Instance.randomEventEffects.TurnOnOffEventEffectServerRpc(false);
-                // Populate objectsToMoveList before conversion
-                gravityStorm.objectsToMoveList.Clear(); // Clear the list before populating
-                foreach (var obj in gravityStorm.objectsToMove)
-                {
-                    gravityStorm.objectsToMoveList.Add(obj);
-                }
+                gravityStorm.objectsToMoveList.Add(obj);
+            }
 
-                gravityStorm.ConvertListToArray();
+            gravityStorm.ConvertListToArray();
 
-                // Stop physics simulation for each object
-                foreach (var obj in gravityStorm.objectsToMove)
+            // Stop physics simulation for each object
+            foreach (var obj in gravityStorm.objectsToMove)
+            {
+                Rigidbody objRigidbody = obj.GetComponent<Rigidbody>();
+                Collider objCollider = obj.GetComponent<Collider>();
+                if (objRigidbody != null)
                 {
-                    Rigidbody objRigidbody = obj.GetComponent<Rigidbody>();
-                    Collider objCollider = obj.GetComponent<Collider>();
-                    if (objRigidbody != null)
-                    {
-                        objCollider.isTrigger = false;
-                        objRigidbody.useGravity = true;
-                        //objRigidbody.Sleep();
-                    }
+                    objCollider.isTrigger = false;
+                    objRigidbody.useGravity = true;
+                    //objRigidbody.Sleep();
                 }
             }
-            if (!randomEvent.GetNetworkObject().IsSpawned)
-            {
-                randomEvent.GetNetworkObject().Spawn();
-            }
-            Debug.LogWarning("DeactivateRandomEvent " + randomEvent.name);
-            gravityButton.GetComponent<MeshRenderer>().material = originalMaterial;
-            randomEvent.SetEventBool(false);
-            randomEvent.ActivateDeactivateEvent();
-            // foreach (GameObject bootParticle in PlayerController.Instance.bootsParticles)
-            // {
-            //     bootParticle.SetActive(GameManager.Instance.isEventActive);
-            // }
         }
 
+        Debug.LogWarning("DeactivateRandomEvent " + randomEvent.name);
+        gravityButton.GetComponent<MeshRenderer>().material = originalMaterial;
+        randomEvent.SetEventBool(false);
+        randomEvent.ActivateDeactivateEvent();
+        // foreach (GameObject bootParticle in PlayerController.Instance.bootsParticles)
+        // {
+        //     bootParticle.SetActive(GameManager.Instance.isEventActive);
+        // }
     }
-
-    
 }
 
